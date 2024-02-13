85006626b3a7f362d134670dd596c2717737a74c
Ivan <name.navi96@gmail.com>
2024-02-13 15:41:38 +0300

    Commit  create Anagram

---
+++ C#/Algorithms_In_C#/Algorithms/PopularStringAlgorithms/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace PopularStringAlgorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool res = Anagram.IsAnagramEnglish("finder", "Friend");
+            var res2 = Anagram.IsAnagramForAllLanguages("пара","Апар");
+            Console.WriteLine($"res-{res}, res2-{res2}");
+        }
+    }
+}
