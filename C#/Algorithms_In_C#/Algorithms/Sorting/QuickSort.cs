c8baadc49e5072d36c6d195ebe565ae438fbf31c
Ivan <name.navi96@gmail.com>
2024-02-27 21:19:50 +0300

    Add QuickSort and SelectionSort

--- C#/Algorithms_In_C#/Algorithms/Sorting/QuickSort.cs
+++ C#/Algorithms_In_C#/Algorithms/Sorting/QuickSort.cs
@@ -1,11 +1,55 @@
-﻿
-namespace Sorting
+﻿namespace Sorting
 {
     public class QuickSort
     {
         public static int[] Sort(int[] sut)
         {
-            throw new NotImplementedException();
+            var arr = sut.ToArray();
+            SortImpl(arr, 0, arr.Length - 1);
+
+            return arr;
+        }
+
+        private static void SortImpl(int[] arr, int lo, int hi)
+        {
+            if(lo >= hi)
+            {
+                return;
+            }
+
+            var p = Partition(arr, lo, hi);
+
+            SortImpl(arr, lo, p);
+            SortImpl(arr, p + 1, hi);
+        }
+
+        private static int Partition(int[] arr, int lo, int hi)
+        {
+            var pivot = arr[lo];
+            var i = lo - 1;
+            var j = hi + 1;
+
+            while(true)
+            {
+                do
+                {
+                    i++;
+                } while(arr[i] < pivot);
+
+                do
+                {
+                    j--;
+                } while(arr[j] > pivot);
+
+                if(i >= j)
+                {
+                    return j;
+                }
+
+                var tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+            }
         }
     }
 }
--- C#/Algorithms_In_C#/Algorithms/Sorting/SelectionSort.cs
+++ C#/Algorithms_In_C#/Algorithms/Sorting/SelectionSort.cs
@@ -5,7 +5,44 @@ namespace Sorting
     {
         public static int[] Sort(int[] sut)
         {
-            throw new NotImplementedException();
+            var array = sut.ToArray();
+            var upperBound = array.Length;
+
+            for(var i = 0; i < upperBound; i++)
+            {
+                var minIndex = i;
+                var maxIndex = upperBound - 1;
+
+                for(var j = i + 1; j < upperBound; j++)
+                {
+                    if(array[j] < array[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                    else if(array[j] > array[maxIndex])
+                    {
+                        maxIndex = j;
+                    }
+                }
+
+                if(minIndex != i)
+                {
+                    var tmp = array[i];
+                    array[i] = array[minIndex];
+                    array[minIndex] = tmp;
+                }
+
+                if(maxIndex != upperBound - 1)
+                {
+                    var tmp = array[upperBound - 1];
+                    array[upperBound - 1] = array[maxIndex];
+                    array[maxIndex] = tmp;
+                }
+
+                upperBound--;
+            }
+
+            return array;
         }
     }
 }
